719fb1a7fb2908ba4a5c385bc58d96382f9587d4
Sahib Arora <sahibarora1997@gmail.com>
2017-09-18 19:50:14 -0400

    Add files via upload

---
+++ Hello_world.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int a, b, c;
+	printf("Hello World!!!");      // its a function which print things in it on the screen, or return things to screen..
+	return 0;      // here we are returning 0 to OS..
+}
